07c6c890b9d360ca3d2f9d9ca66a4a4ab42ddec2
mirinae700 <mirinae219@gmail.com>
2024-11-11 22:54:05 +0900

    feat(application) : main 프로그램 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,42 @@
 package store;
 
+import store.domain.Cart;
+import store.domain.Inventory;
+import store.domain.Order;
+import store.utility.InputView;
+import store.utility.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Inventory inventory = new Inventory();
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+
+        String continueYesOrNo = "Y";
+        while (continueYesOrNo.equals("Y")) {
+            run(inventory, inputView, outputView);
+            continueYesOrNo = null;
+            while (continueYesOrNo == null) {
+                try {
+                    continueYesOrNo = inputView.readContinueYesOrNo();
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    private static void run(Inventory inventory, InputView inputView, OutputView outputView) {
+        outputView.printInventory(inventory);
+        Order order = null;
+        while (order == null) {
+            try {
+                order = new Order(inventory, inputView.readPurchaseData());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        Cart cart = order.addToCart();
+        cart.summary();
     }
 }
